5f1a6ce2109c305f160ac4ace3681cc7fb8ef5f3
HasheebKazi <hasheeb.k@gmail.com>
2019-12-30 10:39:32 -0500

    basic folder structure + css modules

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 className={classes.App}>Hello World!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
