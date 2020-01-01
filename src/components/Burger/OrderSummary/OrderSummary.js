6fb1f0f0ecc811f9bdf8d976d9e686864b8ae0f6
HasheebKazi <hasheeb.k@gmail.com>
2019-12-31 23:02:49 -0500

    finished v1 of app

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // component import
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    
-    const ingredientSummary  = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log('[OrderSummary.js] componentDidUpdate');
+    }
+
+    render() {
+        const ingredientSummary  = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span>{igKey.charAt(0).toUpperCase() + igKey.slice(1)}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
+        
         return (
-            <li key={igKey}>
-                <span>{igKey.charAt(0).toUpperCase() + igKey.slice(1)}</span>: {props.ingredients[igKey]}
-            </li>
-        );
-    });
-
-    
-    return (
-
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>ReAl FOOD with rEaL ingredients from the farm:</p>
-            <ul>
-                { ingredientSummary }
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.cancelPurchase}>Cancel</Button>
-            <Button btnType="Success" clicked={props.continuePurchase}>Continue</Button>
-        </React.Fragment>
-    );
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>ReAl FOOD with rEaL ingredients from the farm:</p>
+                <ul>
+                    { ingredientSummary }
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.cancelPurchase}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.continuePurchase}>Continue</Button>
+            </React.Fragment>
+        );    
+    }
 }
 
 export default OrderSummary;
